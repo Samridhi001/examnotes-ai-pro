afd00111abc27790a46480e5b0275cd8e0bc731f
Samridhi Shandilya <samridhishandilya118@gmail.com>
2026-05-10 15:35:15 +0530

    feat: add login endpoint

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import { User } from "../models/User.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { AppError } from "../utils/AppError.js";
-import { sendCreated } from "../utils/apiResponse.js";
+import { sendCreated, sendSuccess } from "../utils/apiResponse.js";
 import { createAuthToken, setAuthCookie } from "../utils/token.js";
-import { validateRegisterInput } from "../validators/auth.validator.js";
+import { validateLoginInput, validateRegisterInput } from "../validators/auth.validator.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
   const input = validateRegisterInput(req.body);
@@ -29,3 +29,33 @@ export const registerUser = asyncHandler(async (req, res) => {
     "Account created successfully"
   );
 });
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const input = validateLoginInput(req.body);
+
+  const user = await User.findOne({
+    email: input.email
+  }).select("+password");
+
+  if (!user) {
+    throw new AppError("Invalid email or password", 401);
+  }
+
+  const isPasswordCorrect = await user.comparePassword(input.password);
+
+  if (!isPasswordCorrect) {
+    throw new AppError("Invalid email or password", 401);
+  }
+
+  const token = createAuthToken(user._id.toString());
+  setAuthCookie(res, token);
+
+  return sendSuccess(
+    res,
+    {
+      user: user.toPublicJSON()
+    },
+    "Logged in successfully"
+  );
+});
+
