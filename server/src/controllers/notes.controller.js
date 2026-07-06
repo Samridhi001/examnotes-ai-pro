126ea23ec75e0059364390997e0713d90f4b6208
Samridhi Shandilya <samridhishandilya118@gmail.com>
2026-07-06 20:45:38 +0530

    feat: add notes delete and favorite controllers

--- server/src/controllers/notes.controller.js
+++ server/src/controllers/notes.controller.js
@@ -1,7 +1,7 @@
 import { Note } from "../models/Note.js";
 import { generateNotesContent } from "../services/notesGeneration.service.js";
 import { AppError } from "../utils/AppError.js";
-import { sendCreated, sendSuccess } from "../utils/apiResponse.js";
+import { sendCreated, sendError, sendSuccess } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { validateGenerateNotesInput } from "../validators/note.validator.js";
 
@@ -78,3 +78,36 @@ export const deleteNote = asyncHandler(async (req, res) => {
     "Note deleted successfully"
   );
 });
+
+export async function deleteNote(req, res) {
+  const note = await Note.findOneAndDelete({
+    _id: req.params.noteId,
+    user: req.user.id
+  });
+
+  if (!note) {
+    return sendError(res, "Note not found", 404);
+  }
+
+  return sendSuccess(res, "Note deleted successfully", {
+    noteId: req.params.noteId
+  });
+}
+
+export async function toggleFavoriteNote(req, res) {
+  const note = await Note.findOne({
+    _id: req.params.noteId,
+    user: req.user.id
+  });
+
+  if (!note) {
+    return sendError(res, "Note not found", 404);
+  }
+
+  note.isFavorite = !note.isFavorite;
+  await note.save();
+
+  return sendSuccess(res, "Note favorite status updated", {
+    note
+  });
+}
